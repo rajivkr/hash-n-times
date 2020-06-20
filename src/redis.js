3bca02823e271549468307029507d7f07d060b6e
Rajiv K.R <rajiv270992@gmail.com>
2020-06-20 18:55:46 +0530

    including production redux details

--- src/redis.js
+++ src/redis.js
@@ -1,17 +1,23 @@
 import redis from 'redis';
+import Url from 'url';
 
-// 02rCiBWItasw32MeJChDGz9istMh1p4D
-const REDIS_PORT = process.env.REDIS_PORT || 6379;
-const REDIS_HOST = process.env.REDIS_PORT || '127.0.0.1';
-const REDIS_PASS =
-  process.env.REDIS_PORT ||
+const REDISTOGO_URL = process.env.REDISTOGO_URL || undefined;
+let REDIS_PORT = 6379;
+let REDIS_HOST = '127.0.0.1';
+let REDIS_PASS =
   'QPnVuXjVvaKlZ+D8z7z3simO/opiv1wDT9OOorOVG0LPjWB2qjHop1RL2DBIirqUsWPB/XX3rUIAbWeK';
+if (REDISTOGO_URL) {
+  const reduxUrl = Url.parse(process.env.REDISTOGO_URL);
+  REDIS_PORT = reduxUrl.port;
+  REDIS_HOST = reduxUrl.hostname;
+  // eslint-disable-next-line prefer-destructuring
+  REDIS_PASS = reduxUrl.auth.split(':')[1];
+}
 
 const redisClient = redis.createClient(REDIS_PORT, REDIS_HOST, {
   no_ready_check: true,
 });
 redisClient.auth(REDIS_PASS);
-
 redisClient.on('connect', () => {});
 
 export default redisClient;
